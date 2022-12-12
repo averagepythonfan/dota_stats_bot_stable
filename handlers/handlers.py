9c5d5aecbda2f844cd9c27d01451280f1ac4769a
averagepythonfan <djonvorobei@bk.ru>
2022-12-12 14:29:23 +0300

    add full working handler : heroes

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -45,35 +45,49 @@ async def hero_command(message : types.Message):
     pass
 
 async def heroes_command(message : types.Message, patch=7.32, tier_level='premium'):
-    # if len(message.get_args()) == 0:
-    #     args = message.get_args().split()
-    #     patch = float(args[0])
-    #     tier_level = args[1]
+    '''Отправляет дата фрейм с 10 лучшими и 10 худщими героями патча.
 
-    await message.reply('Wait, i\'m working')
+    Параметр: patch - минимальный патч для отсчета статистики герое. Дефолтное значение 7.32
+    Параметр: tier_level - уровень игры для сбора статистики. Доступные значения premuin и professional.
+    '''
+    args = ['start']
+    if len(message.get_args().split()) == 1:
+        args = message.get_args().split()
+        patch = float(args[0])
+    elif len(message.get_args().split()) == 2:
+        args = message.get_args().split()
+        patch = float(args[0])
+        tier_level = args[1]
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://api.opendota.com/api/explorer', params=dict(sql=heroes.format(patch=patch, tier_level=tier_level))) as resp:
-            if resp.status == 200:
-                query = await resp.json()
-                df = pandas.DataFrame(query['rows']).sort_values(by=['winrate_wilson'], ascending=False)
-                df.to_csv('test_df.csv')
-                df = pandas.concat([df.head(10), df.tail(10)])
-                df = df.iloc[:, :5]
-                df = df.rename(columns={'localized_name' : 'name', 'AVG Kills' : 'avg_kills', 'winrate_wilson' : 'win_wil'})
-                df.win_wil = numpy.round(df.win_wil.values, 2) * 100
-                df.avg_kills = numpy.round(df.avg_kills.values.astype(float), 2)
-                df.winrate = numpy.round(df.winrate.values, 4) * 100
-                df = df.reset_index(drop=True)
-                indx = round(time())
-                #df.to_csv(f'df{indx}.csv')
-                dfi.export(df, f'mytable{indx}.png', table_conversion='matplotlib')
-                df_pic = types.InputFile(f'{getcwd()}/mytable{indx}.png')
-                await message.reply_photo(df_pic)            #(f'{getcwd()}/mytable{indx}.png')
-                remove(f'mytable{indx}.png')
-                #await message.reply(str(type(df.avg_kills.values[0])))
-            else:
-                await message.reply('Try again!')
+    if args[0] == 'help':
+        message.reply(heroes_command.__doc__)
+    else:
+        await message.reply('Wait, i\'m working')
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://api.opendota.com/api/explorer', params=dict(sql=heroes.format(patch=patch, tier_level=tier_level))) as resp:
+                if resp.status == 200:
+                    query = await resp.json()
+                    df = pandas.DataFrame(query['rows'])
+                    df = df.iloc[:, :5]
+                    df = df.rename(columns={'localized_name' : 'name', 'AVG Kills' : 'avg_kills', 'winrate_wilson' : 'win_wil', 'count' : 'matches'})
+
+                    df.win_wil = numpy.round(df.win_wil.values, 2) * 100
+                    df.avg_kills = numpy.round(df.avg_kills.values.astype(float), 2)
+                    df.winrate = numpy.round(df.winrate.values, 4) * 100
+
+                    df = df[(df.matches > 30)].sort_values(by=['win_wil'], ascending=False)
+                    df = pandas.concat([df.head(10), df.tail(10)])
+                    df = df.reset_index(drop=True)
+
+                    indx = round(time())
+                    dfi.export(df, f'mytable{indx}.png', table_conversion='matplotlib')
+                    df_pic = types.InputFile(f'{getcwd()}/mytable{indx}.png')
+                    await message.reply_photo(df_pic)
+                    remove(f'mytable{indx}.png')
+                    logger.debug(f'Heroes df pic send to {message.from_user.id}, username {message.from_user.username} at {ctime()}')
+                else:
+                    await message.reply('Try again!')
+                    logger.warning(f'Response equals {resp.status}')
 
     # path_file = types.InputFile('/home/eugene/Coding/dataframe_image/mytable1670520517.png')
     # #onswer = path.exists(path_file)
